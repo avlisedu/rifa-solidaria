4daad8bab8e535f89426379d390bf232fff7588e
Eduardo da Silva <avlis.edu@hotmail.com>
2025-04-08 05:19:26 -0300

    teste de deploy

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -2,9 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import { RifaNumber } from '../types/rifa';
 
+
+// Correto para Vite
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
-
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
